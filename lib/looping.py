172c4355133ff4ab4968a491fa14ff94a2aeefc4
Khalid Sheikh Abdirahman <mac@KHALID.local>
2024-02-27 23:03:52 +0300

    pass test

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,26 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    list_of_squared_elements = [int ** 2 for int in int_list]
+    return list_of_squared_elements
+    
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
